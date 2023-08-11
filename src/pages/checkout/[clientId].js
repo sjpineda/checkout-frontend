56b1582b616721f190d90492968422033bf0918f
zeroluffs <zeroluffs@gmail.com>
2023-08-11 08:15:35 -0500

    location error

--- src/pages/checkout/[clientId].js
+++ src/pages/checkout/[clientId].js
@@ -6,7 +6,6 @@ const stripePromise = loadStripe(
   `pk_test_51J308XAhU6l4hvyfwh3RYZLcTxHmXuXrRwtlu032gGVnfTGsOS16sdjT79xKTZBjwtF2JOsSqAuu2UfcaLjeFSgf00TlSK6Sya`
 )
 export default function Checkout() {
-  const { clientId } = router.query
   const options = {
     clientSecret: clientId,
   }
