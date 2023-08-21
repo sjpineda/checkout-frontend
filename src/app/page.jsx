5617892913b12ba7bc872721eb28d8c2db22f758
Zeroluffs <zeroluffs@gmail.com>
2023-08-21 13:22:42 -0500

    main page form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,90 +1,14 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Form from '@/pages/form'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by &nbsp;
-          <code className={styles.code}>Free Estimate</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-      <a
-          href="/form"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-           Free Estimate
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information.
-          </p>
-        </a>
-       
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="/form"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-           Free Estimate <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information.
-          </p>
-        </a>
-
-        <a
-          href="/form"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-          Free Estimate 
-          </h2>
-          <p className={inter.className}>Explore the  playground.</p>
-        </a>
-
-        <a
-          href="/form"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-          Free Estimate  <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Get your project free Estimate
-          </p>
-        </a>
-      </div>
+    <main >
+     <Form/>
     </main>
   )
 }
